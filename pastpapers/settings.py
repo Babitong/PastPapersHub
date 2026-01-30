a65e8cd0bc7f95178fe3c85f88c0d9fcfafd14a6
Babitong <rexbabitong@gmail.com>
2026-01-30 18:20:12 +0100

    preparing for hosting

--- pastpapers/settings.py
+++ pastpapers/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7l^3a6r86@%4zv6=2jg08hm+u-_&w7f-9a6%-1iw(mb$g+3)!)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
